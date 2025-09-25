be057fcb0c6cd64b0168bad2b3aae611c401c3e0
hongwei <yhw1630@gmail.com>
2025-09-25 10:41:06 -0700

    Simplify agents with backend server api change

--- app/common/pdb/Web3PrivateStorageAgent.js
+++
@@ -1,6 +0,0 @@
-(function(pdb) {
-class Web3PrivateStorageAgent extends pdb.Web3PrivateServerMixin
-(pdb.Web3StorageAgent) {};
-
-pdb.Web3PrivateStorageAgent = Web3PrivateStorageAgent;
-}(window.pdb = window.pdb || {}));
--- app/common/pdb/Web3PublicStorageAgent.js
+++
@@ -1,5 +0,0 @@
-(function(pdb) {
-class Web3PublicStorageAgent extends pdb.Web3StorageAgent {};
-
-pdb.Web3PublicStorageAgent = Web3PublicStorageAgent;
-}(window.pdb = window.pdb || {}));
